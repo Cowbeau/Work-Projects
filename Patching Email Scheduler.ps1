dcd6eebdbd56325f2dfc2b455b145a20d8498982
Beau McCoy <102260964+Cowbeau@users.noreply.github.com>
2023-04-18 20:31:02 -0500

    Add files via upload
    Powershell Script for Email Scheduler

---
+++ Patching Email Scheduler.ps1
@@ -0,0 +1,35 @@
+# Set email parameters
+$smtpServer = "smtp.gmail.com"
+$smtpPort = "587"
+$senderEmail = "your_email@gmail.com"
+$senderPassword = "your_password"
+$receiverEmail = "receiver_email@gmail.com"
+$subject = "Email Subject Goes Here"
+
+# Determine the date of the third Tuesday of the current month
+$date = Get-Date
+$thirdTuesday = Get-Date -Date "$($date.Year)-$($date.Month)-01" | Where-Object { $_.DayOfWeek -eq "Tuesday" } | Select-Object -Index 2
+$thirdTuesdayString = $thirdTuesday.ToString("yyyy-MM-dd")
+
+# Check if today is the third Tuesday of the month and the time is 8:30 am
+if ($date.ToString("yyyy-MM-dd") -eq $thirdTuesdayString -and $date.ToString("HH:mm") -eq "08:30") {
+    # If today is the third Tuesday at 8:30 am, send the email
+    $message = "Send your email message here"
+    $mailParams = @{
+        SmtpServer = $smtpServer
+        Port = $smtpPort
+        UseSSL = $true
+        Credential = New-Object System.Net.NetworkCredential($senderEmail, $senderPassword)
+        From = $senderEmail
+        To = $receiverEmail
+        Subject = $subject
+        Body = $message
+    }
+    try {
+        Send-MailMessage @mailParams
+        Write-Host "Email sent successfully."
+    }
+    catch {
+        Write-Host "An error occurred while sending the email: $_"
+    }
+}
